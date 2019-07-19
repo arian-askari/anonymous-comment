db20182fe92450b90ba7a90c3af7669dc2cb88b9
ArianAskari <diyardownload@gmail.com>
2019-07-19 10:58:58 +0430

    update config for one_to_one method

--- config/config.py
+++ config/config.py
@@ -24,23 +24,40 @@ DATA_DIR = os.sep.join([PROJECT_DIR, "data"])
 # config for data
 
 DATASET_PATH_ORIGINAL = os.sep.join(
-    [PROJECT_DIR, load_project_config()["dataset"]["original"]]
+    [DATA_DIR, load_project_config()["dataset"]["original"]]
 )
+
 DATASET_PATH_A_GOOGLE_USER = os.sep.join(
-    [PROJECT_DIR, load_project_config()["dataset"]["a_google_user"]]
+    [DATA_DIR, load_project_config()["dataset"]["a_google_user"]]
 )
+
 DATASET_PATH_ONE_TO_ONE = os.sep.join(
-    [PROJECT_DIR, load_project_config()["dataset"]["one_to_one"]]
+    [DATA_DIR, load_project_config()["dataset"]["one_to_one"]]
 )
+
 DATASET_PATH_MACHINE_TRANSLATION = os.sep.join(
-    [PROJECT_DIR, load_project_config()["dataset"]["machine_translation"]]
+    [DATA_DIR, load_project_config()["dataset"]["machine_translation"]]
 )
+
 DATASET_PATH_OBFUSCATE = os.sep.join(
-    [PROJECT_DIR, load_project_config()["dataset"]["obfuscate"]]
+    [DATA_DIR, load_project_config()["dataset"]["obfuscate"]]
 )
+
 GREEDY_DATASET_PATH = os.sep.join(
-    [PROJECT_DIR, load_project_config()["dataset"]["random"]]
+    [DATA_DIR, load_project_config()["dataset"]["random"]]
 )
+
 RANDOM_DATASET_PATH = os.sep.join(
-    [PROJECT_DIR, load_project_config()["dataset"]["greedy"]]
+    [DATA_DIR, load_project_config()["dataset"]["greedy"]]
+)
+
+
+# config for algorithms - one-to-one
+
+ONE_TO_ONE_RETRY_NUMBER = int(
+    load_project_config()["one_to_one"]["retry_number"]
+)
+
+ONE_TO_ONE_PERCENTAGE = int(
+    load_project_config()["one_to_one"]["retry_number"]
 )
